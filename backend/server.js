ef2a0cb317a30f6239a7c9e1802a38268ceb4f33
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-22 11:31:33 +0530

    backend: Express route setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,26 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.json({msg:"Get all workouts"})
+})
+
+app.get('/:id',(req,res)=>{
+    res.json({msg:"Get single workout"})
+})
+
+app.post('/',(req,res)=>{
+    res.json({msg:"Create a new workout"})
+})
+
+app.delete('/:id',(req,res)=>{
+    res.json({msg:"Delete single workout"})
+})
+
+app.patch('/:id',(req,res)=>{
+    res.json({msg:"Update single workout"})
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`listening on port ${process.env.PORT}`)
 })
