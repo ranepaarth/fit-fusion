6d2970d1016b4f57c96d30675d3af925f053ca23
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-22 19:33:24 +0530

    backend: parsing JSON data using middleware

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,16 @@ const mongoose = require("mongoose");
 const app = express();
 const workoutRouter = require("./router/workoutRouter");
 
+app.use(express.json())
 app.use("/api/workout", workoutRouter);
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`listening on port ${process.env.PORT}`);
-    });
+   console.log('connected to db successfully');
+   app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+  });
   })
   .catch((err) => console.log(err));
+   
