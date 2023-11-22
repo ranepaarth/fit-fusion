67cfb4c232dfe3fbec49c49e94bf9d467ca6bc22
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-22 11:55:57 +0530

    backend: workoutRouter: refact

--- backend/router/workoutRouter.js
+++ backend/router/workoutRouter.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.json({msg:"Get all workouts"})
-})
+router
+  .get("/", (req, res) => {
+    res.json({ msg: "Get all workouts" });
+  })
+  .post("/", (req, res) => {
+    res.json({ msg: "Create a new workout" });
+  });
 
-router.get('/:id(\\d+)',(req,res)=>{
-    res.json({msg:"Get single workout"})
-})
+router
+  .get("/:id(\\d+)", (req, res) => {
+    res.json({ msg: "Get single workout" });
+  })
+  .delete("/:id(\\d+)", (req, res) => {
+    res.json({ msg: "Delete single workout" });
+  })
+  .patch("/:id(\\d+)", (req, res) => {
+    res.json({ msg: "Update single workout" });
+  });
 
-router.post('/',(req,res)=>{
-    res.json({msg:"Create a new workout"})
-})
-
-router.delete('/:id(\\d+)',(req,res)=>{
-    res.json({msg:"Delete single workout"})
-})
-
-router.patch('/:id(\\d+)',(req,res)=>{
-    res.json({msg:"Update single workout"})
-})
-
-module.exports = router
+module.exports = router;
