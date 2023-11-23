2c615d723d11f0385cf15a016cbf2b619d888ccf
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-23 19:44:15 +0530

    frontend: proxy setup

--- frontend/src/components/WorkoutsSection.jsx
+++ frontend/src/components/WorkoutsSection.jsx
@@ -4,7 +4,7 @@ const WorkoutsSection = () => {
   const [workouts, setWorkouts] = useState(null);
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("http://localhost:4000/api/workout");
+      const response = await fetch("/api/workout");
       const data = await response.json();
 
       if (response.ok) setWorkouts(data);
@@ -14,7 +14,7 @@ const WorkoutsSection = () => {
   }, []);
 
   return (
-    <section className="flex flex-col gap-4 w-full mt-8">
+    <section className="flex flex-col gap-4 w-full md:w-[60%] mt-8">
       {workouts?.map((workout) => {
           return <WorkoutCard workout={workout} key={workout._id}/>;
         })}
