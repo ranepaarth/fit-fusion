011769dd93002d98dbeb9a444ea52f633a9fab92
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-23 19:20:38 +0530

    frontend: react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <RootLayout />,
+    children:[
+      {
+        path:'/',
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path:'*',
+    element: <h1>Error</h1>
+  }
+])
 
 function App() {
-
   return (
-    <h1 className="underline">Fit fusion</h1>
+   <RouterProvider router={router}/>
   )
 }
 
