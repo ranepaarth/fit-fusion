1e325582e4e585be22e79d5a8bd6b80dc09bdd28
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-24 21:05:49 +0530

    frontend: inserted logo

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoBarbell } from "react-icons/io5";
+const Logo = () => {
+  return (
+    <div>
+      <span className="text-5xl">
+        <IoBarbell className=" bg-blue-500 p-2 rounded-full"/>
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import {Logo} from '../componentsRoute.js'
 const Navbar = () => {
   return (
     <nav className="bg-neutral-700 h-20 flex items-center px-[5%] lg:px-[5%]">
-      <NavLink to='/'>
-        <h1 className="text-4xl font-semibold cursor-pointer">Fit Fusion</h1>
+      <NavLink to='/' className='flex items-center gap-2'>
+        <Logo />
+        <h1 className="text-4xl font-bold cursor-pointer hidden md:block">Fit Fusion</h1>
       </NavLink>
     </nav>
   );
