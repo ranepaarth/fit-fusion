0dcf3d3fe5ebc81a6cff3b41881e504e7efe5aaa
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-24 21:13:10 +0530

    frontend: context with reducer function

--- frontend/src/context/WorkoutContext.jsx
+++ frontend/src/context/WorkoutContext.jsx
@@ -1,15 +1,88 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 
 const WorkoutContext = createContext();
 
+export const workoutReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter(
+          (workout) => workout._id != action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+const initialWorkout = {
+  workouts: null,
+};
+
 export const WorkoutContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutReducer, initialWorkout);
+  const [title, setTitle] = useState("");
+  const [reps, setReps] = useState("");
+  const [sets, setSets] = useState("");
+  const [load, setLoad] = useState("");
+  const [error, setError] = useState(null);
+
+  const fetchAllWorkouts = (workouts) => {
+    return dispatch({ type: "SET_WORKOUTS", payload: workouts });
+  };
+
+  const fetchCreatedWorkout = (workout) => {
+    return dispatch({ type: "CREATE_WORKOUT", payload: workout });
+  };
+
+  const deleteSingleWorkout = (workoutId) => {
+    console.log(workoutId);
+    return dispatch({ type: "DELETE_WORKOUT", payload: workoutId });
+  };
+
+  const resetStates = () => {
+    setError(null);
+    setTitle("");
+    setLoad("");
+    setSets("");
+    setReps("");
+  };
+
   return (
-    <WorkoutContext.Provider value={"paarth"}>
+    <WorkoutContext.Provider
+      value={{
+        ...state,
+        fetchAllWorkouts,
+        fetchCreatedWorkout,
+        deleteSingleWorkout,
+        title,
+        setTitle,
+        reps,
+        setReps,
+        sets,
+        setSets,
+        load,
+        setLoad,
+        error,
+        setError,
+        resetStates
+      }}
+    >
       {children}
     </WorkoutContext.Provider>
   );
 };
 
-export const useWorkoutContext = () => {
+const useWorkoutContext = () => {
   return useContext(WorkoutContext);
 };
+
+export default useWorkoutContext
