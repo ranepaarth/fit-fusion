7cab6a202b91ea48accc255d53a2888a40ce91ce
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-26 20:16:13 +0530

    frontend: date-fns

--- frontend/src/components/WorkoutCard/WorkoutCreatedAt.jsx
+++ frontend/src/components/WorkoutCard/WorkoutCreatedAt.jsx
@@ -1,17 +1,10 @@
-import moment from "moment";
 import React from "react";
-
+import {formatDistance} from 'date-fns'
 const WorkoutCreatedAt = ({ createdAt }) => {
-  const formatter = new Intl.DateTimeFormat("en-GB", {
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  });
 
   return (
     <span className="text-neutral-400 text-base font-normal flex items-center gap-1">
-      <p>{formatter.format(Date.parse(createdAt))} at </p>
-      <p>{moment(createdAt).format("hh:mm A")}</p>
+      <p>{formatDistance(new Date(createdAt),new Date(),{addSuffix: true})}</p>
     </span>
   );
 };
