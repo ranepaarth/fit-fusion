37fd1d114a019a490fb356ff46306e06cca06730
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-11 21:52:01 +0530

    frontend: improved code and minor style changes

--- frontend/src/components/WorkoutsSection.jsx
+++ frontend/src/components/WorkoutsSection.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect } from "react";
-import { WorkoutCard, useWorkoutContext } from "../componentsRoute";
+import {
+  EmptyWorkoutCard,
+  WorkoutCard,
+  useWorkoutContext,
+} from "../componentsRoute";
 const WorkoutsSection = () => {
   // const [workouts, setWorkouts] = useState(null);
-  const {workouts,fetchAllWorkouts} = useWorkoutContext()
+  const { workouts, fetchAllWorkouts } = useWorkoutContext();
   useEffect(() => {
     const fetchWorkouts = async () => {
+      try {
+        const response = await fetch("/api/workout");
 
-      const response = await fetch("/api/workout");
+        const data = await response.json();
 
-      const data = await response.json();
-
-      if (response.ok) fetchAllWorkouts(data)
+        if (response.ok) {
+          fetchAllWorkouts(data);
+          // setError(false);
+        }
+        // console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
     };
-
     fetchWorkouts();
   }, []);
 
   return (
-
-    <section className="flex flex-col gap-4 w-full md:w-[50%] mt-8">
+    <section className="flex flex-col gap-4 w-full md:w-[50%]">
       {workouts?.map((workout) => {
-          return <WorkoutCard workout={workout} key={workout._id}/>;
-        })}
+        return <WorkoutCard workout={workout} key={workout._id} />;
+      })}
+
+      {workouts?.length === 0 ? <EmptyWorkoutCard /> : ""}
     </section>
   );
 };
