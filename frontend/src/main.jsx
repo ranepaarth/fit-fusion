fadebc9c6dd3d341a585b04c7b986516ed152f36
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-11 21:56:18 +0530

    frontend: auth and ref context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import AuthContextProvider from "./context/AuthContext.jsx";
 import { DarkModeContextProvider } from "./context/DarkModeContext.jsx";
+import RefContextProvider from "./context/RefContext.jsx";
 import { WorkoutContextProvider } from "./context/WorkoutContext.jsx";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <DarkModeContextProvider>
-        <App />
-      </DarkModeContextProvider>
-    </WorkoutContextProvider>
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <RefContextProvider>
+          <DarkModeContextProvider>
+            <App />
+          </DarkModeContextProvider>
+        </RefContextProvider>
+      </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
