e96cb7c65c168b0c7f2a91faf19f6cc101ebe138
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-11 21:50:39 +0530

    frontend: changed the theme toggle ui

--- frontend/src/components/Navbar/DarkModeToggleBtn.jsx
+++ frontend/src/components/Navbar/DarkModeToggleBtn.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { moonIcon, sunIcon, useDarkModeContext } from '../../componentsRoute';
-
+import React from "react";
+import { IoMoonOutline, IoSunny } from "react-icons/io5";
+import { useDarkModeContext } from "../../componentsRoute";
 const DarkModeToggleBtn = () => {
-    const { darkMode, toggleDarkMode } = useDarkModeContext();
+  const { darkMode, toggleDarkMode } = useDarkModeContext();
   return (
- <button
+    <span
+      className="flex items-center gap-2 px-4 py-2 my-2 dark:text-neutral-200 text-blue-500 text-base cursor-pointer font-sans dark:hover:bg-neutral-800 hover:bg-blue-200"
+      title="Toggle Theme"
+      onClick={toggleDarkMode}
+      onKeyDown={toggleDarkMode}
+    >
+      {/* <button
         onClick={toggleDarkMode}
         className={darkMode ? "sun-icon" : "moon-icon"}
         title={darkMode ? "Light Mode" : "Dark Mode"}
       >
         {darkMode ? (
-          <img src={sunIcon} alt="" className="w-5"/>
+          <img src={sunIcon} alt="" className="w-5" />
         ) : (
-          <img src={moonIcon} alt="" className="w-5 mix-blend-multiply"/>
+          <img src={moonIcon} alt="" className="w-5 mix-blend-multiply" />
         )}
-      </button>
-  )
-}
+      </button> */}
 
-export default DarkModeToggleBtn
+      <button className="text-xl">
+        {darkMode ? <IoMoonOutline /> : <IoSunny />}
+      </button>
+      <p className="">
+        Appearance: {darkMode ? "Dark Theme" : "Light Theme"}
+      </p>
+    </span>
+  );
+};
+
+export default DarkModeToggleBtn;
