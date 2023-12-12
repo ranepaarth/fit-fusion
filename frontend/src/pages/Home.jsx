d1f1b52c055526e427e45f49681f9503240da95c
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-12 20:37:17 +0530

    frontend: changes due to prettier code formatter

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { WorkoutsForm, WorkoutsSection } from '../componentsRoute'
+import React from "react";
+import { WorkoutsForm, WorkoutsSection } from "../componentsRoute";
 
 const Home = () => {
-  console.log('rendered')
+  console.log("rendered");
   return (
-    <div className='font-medium text-xl flex items-center max-md:flex-col-reverse md:flex-row md:items-start md:justify-between md: px-[5%] py-[5%]'>
-        <WorkoutsSection />
-        <WorkoutsForm />
+    <div className="font-medium text-xl flex items-center max-md:flex-col-reverse md:flex-row md:items-start md:justify-between md: px-[5%] py-[5%]">
+      <WorkoutsSection />
+      <WorkoutsForm />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
