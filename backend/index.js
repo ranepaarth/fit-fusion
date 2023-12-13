53af0659fbb604de83ca9f6d9df0e3b5e96362c6
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-13 13:29:37 +0530

    backend: allowing cross origin access

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,25 @@ const app = express();
 const workoutRouter = require("./router/workoutRouter");
 const userRouter = require("./router/userRouter");
 
-app.use((req,res,next)=>{
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  next()
-})
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use("/api/workout", workoutRouter);
