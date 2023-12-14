2f8f7b9aec057b35a51c590e79bc2ce8ed052c9c
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-14 21:17:19 +0530

    frontend: test user info displayed

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import { LuCopy, LuCopyCheck } from "react-icons/lu";
 import { MdLockOutline, MdOutlineMailOutline } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { Logo } from "../componentsRoute";
@@ -12,12 +13,28 @@ const LogIn = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isCopied, setIsCopied] = useState(null);
   const { user } = useAuthContext();
   const { logIn, error, isLoading } = useLogIn();
   const ref = useRefContext();
   useEffect(() => {
     ref.current.focus();
   }, []);
+  const emailRef = useRef();
+  const pwdRef = useRef();
+
+  const handleEmailCopied = () => {
+    setIsCopied(false);
+    window.navigator.clipboard.writeText(emailRef.current?.innerText);
+    console.log(emailRef.current);
+    setIsCopied(true);
+  };
+  const handlePwdCopied = () => {
+    setIsCopied(false);
+    window.navigator.clipboard.writeText(pwdRef.current?.innerText);
+    console.log(pwdRef.current);
+    setIsCopied(true);
+  };
 
   const togglePassword = () => {
     setShowPassword(!showPassword);
@@ -27,6 +44,7 @@ const LogIn = () => {
     e.preventDefault();
     await logIn(userName, password);
   };
+  console.log(isCopied);
   return (
     <div className="user-form-container">
       <form className="user-form-body" onSubmit={handleSubmit}>
@@ -43,7 +61,40 @@ const LogIn = () => {
             </NavLink>
           </p>
         </span>
-
+        <div className="flex flex-col items-center gap-2 font-medium text-lg bg-neutral-300 dark:bg-neutral-500 mx- px-4 py-2 rounded mx-10">
+          <span className="flex flex-row items-center w-full justify-between">
+            <p className="text-neutral-800 dark:text-neutral-300">Email:</p>
+            <p className="text-blue-700 dark:text-blue-400" ref={emailRef}>
+              test3@test.com
+            </p>
+            <button
+              type="button"
+              onClick={handleEmailCopied}
+              className={
+                "text-neutral-800 dark:text-neutral-300 dark:focus:text-green-400 focus:text-green-600"
+              }
+              id="email-btn"
+            >
+              {<LuCopy />}
+            </button>
+          </span>
+          <span className="flex items-center justify-between gap-2 w-full">
+            <p className="text-neutral-800 dark:text-neutral-300">Password:</p>
+            <p className="text-blue-700 dark:text-blue-400" ref={pwdRef}>
+              Test@123
+            </p>
+            <button
+              type="button"
+              className={
+                "text-neutral-800 dark:text-neutral-300 dark:focus:text-green-400 focus:text-green-600"
+              }
+              id="pwd-btn"
+              onClick={handlePwdCopied}
+            >
+              <LuCopy />
+            </button>
+          </span>
+        </div>
         <div className="user-input-row">
           <label htmlFor="username" className="user-input-label">
             <MdOutlineMailOutline />
