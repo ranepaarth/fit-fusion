319384f834b761e902968b85be98a21bd4ced8c5
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-16 12:48:06 +0530

    backend: cors config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const workoutRouter = require("./router/workoutRouter");
@@ -8,21 +8,26 @@ const userRouter = require("./router/userRouter");
 
 // Add headers
 app.use(function (req, res, next) {
-
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader("Access-Control-Allow-Origin",
+    process.env.CLIENT_URL);
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Pass to next layer of middleware
   next();
 });
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/api/workout", workoutRouter);
 app.use("/api/user", userRouter);
